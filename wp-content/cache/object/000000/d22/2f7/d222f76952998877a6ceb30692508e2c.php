9faf90fcc7037446d45c2287c3b00e3f0fa48908
Geoff Graham <geoff@geoffgraham.me>
2013-11-11 16:38:10 -0800

    Update Contact Form 7 plugin

--- wp-content/cache/object/000000/d22/2f7/d222f76952998877a6ceb30692508e2c.php
+++ wp-content/cache/object/000000/d22/2f7/d222f76952998877a6ceb30692508e2c.php
@@ -1,1 +1,1 @@
-şD}R<?php exit; ?>a:1:{s:7:"content";O:8:"stdClass":4:{s:12:"last_checked";i:1383941194;s:7:"checked";a:1:{s:15:"GeoffGrahamFour";s:3:"1.0";}s:8:"response";a:0:{}s:12:"translations";a:0:{}}}
+uR<?php exit; ?>a:1:{s:7:"content";O:8:"stdClass":4:{s:12:"last_checked";i:1384215645;s:7:"checked";a:1:{s:15:"GeoffGrahamFour";s:3:"1.0";}s:8:"response";a:0:{}s:12:"translations";a:0:{}}}
--- wp-content/w3tc-config/master.php
+++ wp-content/w3tc-config/master.php
@@ -653,7 +653,7 @@ return array(
 	'notes.wp_content_perms' => true,
 	'notes.theme_changed' => false,
 	'notes.wp_upgraded' => false,
-	'notes.plugins_updated' => false,
+	'notes.plugins_updated' => true,
 	'notes.cdn_upload' => false,
 	'notes.cdn_reupload' => false,
 	'notes.need_empty_pgcache' => false,
