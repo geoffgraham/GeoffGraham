ac1b2153827f0744c94d27f7f90f6f9c8c55d92f
Geoff Graham <geoff@geoffgraham.me>
2014-02-03 07:45:21 -0800

    Added GA event tracking to email newsletter

--- wp-content/themes/GeoffGrahamFour/sidebar-email-newsletter.php
+++ wp-content/themes/GeoffGrahamFour/sidebar-email-newsletter.php
@@ -22,7 +22,7 @@
           <input id="fieldEmail" placeholder="email@example.com" name="cm-ttykjjj-ttykjjj" type="email" pattern="[^ @]*@[^ @]*" required />
         </p>
         <p>
-          <button type="submit">Subscribe</button>
+          <button type="submit" onClick="_gaq.push(['_trackEvent', 'Email', 'Signup', 'Devigner Digest'])">Subscribe</button>
         </p>
       </form>
     </div>
