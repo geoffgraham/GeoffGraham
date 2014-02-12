39bac542bfbd1a106f0897e4b634c8f17b79e409
Geoff Graham <geoff@geoffgraham.me>
2014-02-12 09:39:55 -0800

    Updated order of enqueued scripts

--- wp-content/themes/GeoffGrahamFour/lib/functions/enqueue-scripts.php
+++ wp-content/themes/GeoffGrahamFour/lib/functions/enqueue-scripts.php
@@ -5,32 +5,24 @@
  * Reference: http://codex.wordpress.org/Function_Reference/wp_enqueue_script
  * ----------------------------------------------------------------------------
  */
- 
-function geoffgraham_scripts_styles() {
-	global $wp_styles;
 
-	// Load Comments	
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
-
-	// Load Stylesheet
-  wp_enqueue_style('global.css', get_template_directory_uri().'/lib/styles/global.css', false ,'1.0', 'all' );
-
-	// Load Scripts
-	wp_enqueue_script( 'geoffgraham-modernizr', get_template_directory_uri() . '/lib/javascripts/vendor/custom.modernizr-min.js' ); 
-	wp_enqueue_script( 'geoffgraham-global', get_template_directory_uri() . '/lib/javascripts/global-min.js', array(), '1.0.0', false );
-  wp_enqueue_script( 'fitvids', get_template_directory_uri() . '/lib/javascripts/vendor/jquery.fitvids.js', array(), '1.0.0', true );
-}
-add_action( 'wp_enqueue_scripts', 'geoffgraham_scripts_styles' );
-
-// Load jQuery
-if ( !function_exists( 'core_mods' ) ) {
-	function core_mods() {
-		if ( !is_admin() ) {
+function geoffgraham_scripts() {
+	
+	// Load stylesheet
+	wp_enqueue_style('global.css', get_template_directory_uri().'/lib/styles/global.css' );
+	
+	// Load Modernizr
+	wp_enqueue_script( 'modernizr.js', get_template_directory_uri() . '/lib/javascripts/vendor/custom.modernizr-min.js', array(), '1.0.0', false );
+	
+	// Register and load jquery
+	if ( !is_admin() ) {
 			wp_deregister_script( 'jquery' );
 			wp_register_script( 'jquery', ( "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ), array(), '1.9.1', false);
 			wp_enqueue_script( 'jquery' );
-		}
-	}
-	add_action( 'wp_enqueue_scripts', 'core_mods' );
+  }
+	
+	// Load custom scripts
+	wp_enqueue_script( 'global.js', get_template_directory_uri() . '/lib/javascripts/global-min.js', array(), '1.0.0', false );
 }
+
+add_action( 'wp_enqueue_scripts', 'geoffgraham_scripts' );
--- wp-content/themes/GeoffGrahamFour/lib/javascripts/global.js
+++ wp-content/themes/GeoffGrahamFour/lib/javascripts/global.js
@@ -1,40 +1,22 @@
-// CONTENTS
-// -----------
+// GLOBAL JAVASCRIPTS
+// ------------------
 
-// Responsive Menu
-// Animate Bar Chart
-// Lettering.js
-// FitText.js
+// @codekit-append fittext.js
+// @codekit-append lettering.js
 
-// Responsive Menu
+// Main Menu Toggle
 $(document).ready(function() {
   $('body').addClass('js');
   var $menu = $('#menu'),
     $menulink = $('.menu-icon');
   
-$menulink.click(function() {
-  $menulink.toggleClass('active');
-  $menu.toggleClass('active');
-  return false;
-});});
+  // On clock, add .active
+  $menulink.click(function() {
+    $menulink.toggleClass('active');
+    $menu.toggleClass('active');
+    return false;
+  });
 
-// Back to Top with smooth scroll
-jQuery(document).ready(function() {
-    var offset = 220;
-    var duration = 500;
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.back-to-top').fadeIn(duration);
-        } else {
-            jQuery('.back-to-top').fadeOut(duration);
-        }
-    });
-    
-    jQuery('.back-to-top').click(function(event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, duration);
-        return false;
-    });
 });
 
 // Animate Bar Chart
@@ -71,126 +53,25 @@ $(document).ready(function(){
   });
 });
 
-// Animate Progress Bars
-// on page load...
-    moveProgressBar();
-    // on browser resize...
-    $(window).resize(function() {
-        moveProgressBar();
-    });
-
-    // SIGNATURE PROGRESS
-    function moveProgressBar() {
-      console.log("moveProgressBar");
-        var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
-        var getProgressWrapWidth = $('.progress-wrap').width();
-        var progressTotal = getPercent * getProgressWrapWidth;
-        var animationLength = 1500;
-        
-        // on page load, animate percentage bar to data percentage length
-        // .stop() used to prevent animation queueing
-        $('.progress-bar').stop().animate({
-            left: progressTotal
-        }, animationLength);
-    }
-
-// Lettering.JS 0.6.1
-// Copyright 2010, Dave Rupert http://daverupert.com
-// Released under the WTFPL license 
-// Date: Mon Sep 20 17:14:00 2010 -0600
-
-(function($){
-	function injector(t, splitter, klass, after) {
-		var a = t.text().split(splitter), inject = '';
-		if (a.length) {
-			$(a).each(function(i, item) {
-				inject += '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
-			});
-			t.empty().append(inject);
-		}
-	}
-	
-	var methods = {
-		init : function() {
-
-			return this.each(function() {
-				injector($(this), '', 'char', '');
-			});
-
-		},
-
-		words : function() {
-
-			return this.each(function() {
-				injector($(this), ' ', 'word', ' ');
-			});
-
-		},
-		
-		lines : function() {
-
-			return this.each(function() {
-				var r = "eefec303079ad17405c889e092e105b0";
-				// Because it's hard to split a <br/> tag consistently across browsers,
-				// (*ahem* IE *ahem*), we replace all <br/> instances with an md5 hash 
-				// (of the word "split").  If you're trying to use this plugin on that 
-				// md5 hash string, it will fail because you're being ridiculous.
-				injector($(this).children("br").replaceWith(r).end(), r, 'line', '');
-			});
-
-		}
-	};
-
-	$.fn.lettering = function( method ) {
-		// Method calling logic
-		if ( method && methods[method] ) {
-			return methods[ method ].apply( this, [].slice.call( arguments, 1 ));
-		} else if ( method === 'letters' || ! method ) {
-			return methods.init.apply( this, [].slice.call( arguments, 0 ) ); // always pass an array
-		}
-		$.error( 'Method ' +  method + ' does not exist on jQuery.lettering' );
-		return this;
-	};
-
-})(jQuery);
-
-// FitText.js 1.1
-// Copyright 2011, Dave Rupert http://daverupert.com
-// Released under the WTFPL license
-// Date: Thu May 05 14:23:00 2011 -0600
-
-(function( $ ){
-
-  $.fn.fitText = function( kompressor, options ) {
-
-    // Setup options
-    var compressor = kompressor || 1,
-        settings = $.extend({
-          'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
-
-    return this.each(function(){
-
-      // Store the object
-      var $this = $(this);
-
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-    });
-
-  };
-
-})( jQuery );
+// Horizontal Bar Chart
+moveProgressBar();
+// on browser resize...
+$(window).resize(function() {
+  moveProgressBar();
+});
+// Signature progress
+function moveProgressBar() {
+    console.log("moveProgressBar");
+    var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
+    var getProgressWrapWidth = $('.progress-wrap').width();
+    var progressTotal = getPercent * getProgressWrapWidth;
+    var animationLength = 1500;
+    // on page load, animate percentage bar to data percentage length
+    // .stop() used to prevent animation queueing
+    $('.progress-bar').stop().animate({
+        left: progressTotal
+  }, animationLength);
+}
 
 // FitVids
 $(document).ready(function(){
