5acda26f17dc42946e35a119603705ab0fe356a7
Geoff Graham <geoff@geoffgraham.me>
2014-05-21 13:18:22 -0700

    Cleans up <head> formatting in header.php

--- wp-content/themes/GeoffGrahamFour/header.php
+++ wp-content/themes/GeoffGrahamFour/header.php
@@ -17,9 +17,9 @@
 
 	<meta charset="<?php bloginfo('charset'); ?>">
 	
-	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
-	<!--[if IE ]>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<?php // Always force latest IE rendering engine (even in intranet) & Chrome Frame ?>
+<!--[if IE ]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<![endif]-->
 
 	<?php if (is_search()) echo '<meta name="robots" content="noindex, nofollow" />'; ?>
@@ -28,44 +28,34 @@
 
 	<meta name="title" content="<?php wp_title( '|', true, 'right' ); ?>">
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
-	<link rel="shortcut icon" href="http://localhost:8888/wp-content/themes/GeoffGrahamFour/lib/images/favicon.ico" />
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144x144-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/apple-touch-icon-57x757-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png" />
-  <link rel="author" href="https://plus.google.com/+GeoffGraham/posts" />
-	
-
 	<?php if (true == of_get_option('meta_author')) echo '<meta name="author" content="'.of_get_option("meta_author").'" />'; ?>
-	
-	<?php if (true == of_get_option('meta_google')) echo '<meta name="google-site-verification" content="'.of_get_option("meta_google").'" />'; ?>
-
 	<meta name="Copyright" content="Copyright &copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All Rights Reserved.">
-
 	<?php if (true == of_get_option('meta_viewport')) {
-	echo '<meta name="viewport" content="'.of_get_option("meta_viewport").'" />';
-	echo '<!--  Mobile Viewport Fix
-	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
-	device-width : Occupy full width of the screen in its current orientation
-	initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
-	maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
-	-->';
+	  echo '<meta name="viewport" content="'.of_get_option("meta_viewport").'" />';
 	} ?>
-
+	
+	
+	<link rel="author" href="https://plus.google.com/+GeoffGraham/posts" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	
-	<!-- Fonts -->
-  <script type="text/javascript">
-  (function() {
-    var config = {
-      kitId: 'rwm5lvr',
-      scriptTimeout: 3000
-    };
-    var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
-  })();
-  </script>
+	<link rel="shortcut icon" href="http://localhost:8888/wp-content/themes/GeoffGrahamFour/lib/images/favicon.ico" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144x144-precomposed.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114x114-precomposed.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72x72-precomposed.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="/apple-touch-icon-57x757-precomposed.png" />
+        <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png" />
+	
+    <?php // Typekit ?>
+    <script type="text/javascript">
+        (function() {
+          var config = {
+            kitId: 'rwm5lvr',
+            scriptTimeout: 3000
+          };
+          var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
+        })();
+        </script>
 
 	<?php wp_head(); ?>
 
