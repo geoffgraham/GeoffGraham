38d78fe1c0abc2a72f25c479ec7181386cc608be
geoffgraham <geoff@geoffgraham.me>
2020-01-02 14:48:53 -0800

    Removes “role” from header element.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- header.php
+++ header.php
@@ -24,8 +24,8 @@
 
 <body <?php body_class(); ?>>
 
-	<header class="site-header" role="header">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'geoff-graham' ); ?></a>
+	<header class="site-header">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'geoff-graham' ); ?></a>
 		<?php get_template_part( 'template-parts/header/navigation', 'title' ); ?>
 		<div class="site-navigation">
 			<?php get_template_part( 'template-parts/header/navigation', 'main' ); ?>
