fd3c3abea0da9d8e1a257aaaf2599d3ca0ca1cbe
geoffgraham <geoff@geoffgraham.me>
2020-01-02 15:31:47 -0800

    Setting aria-label on client links.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- template-parts/content/content-clients.php
+++ template-parts/content/content-clients.php
@@ -1,25 +1,25 @@
 <aside class="work-places">
 		<h3><span>Where you can find me</span></h3>
 		<div class="work-places__items">
-			<a class="work-places__item" href="https://tri.be">
+			<a class="work-places__item" href="https://tri.be" aria-label="External link to Modern Tribe website.">
 				<article>
 					<?php echo file_get_contents( get_template_directory_uri() . '/dist/img/modern-tribe-logo.svg' ); ?>
 					<h4>Modern Tribe</h4>
 				</article>
 			</a>
-			<a class="work-places__item" href="https://css-tricks.com/author/geoffgraham/">
+			<a class="work-places__item" href="https://css-tricks.com/author/geoffgraham/" aria-label="External link to my author page on CSS-Tricks">
 				<article>
 				<?php echo file_get_contents( get_template_directory_uri() . '/dist/img/css-tricks-logo.svg' ); ?>
 				<h4>CSS-Tricks</h4>
 				</article>
 			</a>
-			<a class="work-places__item" href="https://codepen.io/geoffgraham">
+			<a class="work-places__item" href="https://codepen.io/geoffgraham" aria-label="External link to CodePen.">
 				<article>
 					<?php echo file_get_contents( get_template_directory_uri() . '/dist/img/codepen-logo.svg' ); ?>
 					<h4>CodePen</h4>
 				</article>
 			</a>
-			<a class="work-places__item" href="/contact">
+			<a class="work-places__item" href="/contact" aria-label="Link to my contact form.">
 				<article>
 					<?php echo file_get_contents( get_template_directory_uri() . '/dist/img/toilet.svg' ); ?>
 					<h4>Brainstorming</h4>
