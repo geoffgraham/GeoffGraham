1905c6d60830d7d9c1a4a1a223b23e9402b683b6
geoffgraham <geoff@geoffgraham.me>
2020-03-14 21:56:55 -0700

    Adds aria-label to footer logo.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 			</div>
 			<div class="site-footer__nav" role="navigation" aria-label="Footer Links">
 				<?php get_template_part( 'template-parts/header/navigation', 'social' ); ?>
-				<a href="<?php echo home_url(); ?> "><?php echo file_get_contents( get_template_directory_uri() . '/dist/img/logo-geoff.svg' ); ?></a>
+				<a href="<?php echo home_url(); ?>" aria-label="Return Home"><?php echo file_get_contents( get_template_directory_uri() . '/dist/img/logo-geoff.svg' ); ?></a>
 			</div>
 		</footer>
 
