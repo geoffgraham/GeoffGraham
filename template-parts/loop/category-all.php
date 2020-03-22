10c38882e203220bd1c6922107bcd7b020c3e57f
geoffgraham <geoff@geoffgraham.me>
2020-03-21 17:16:59 -0700

    Fixes some funky inconsistencies with headers and overflow on post archive.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- template-parts/loop/category-all.php
+++ template-parts/loop/category-all.php
@@ -1,7 +1,8 @@
 <article class="post">
 
 	<div class="post__date"><?php the_date( 'M d, Y' );?></div>
-	<?php the_title( '<div class="post__title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></div>' );
-?>
+	<?php 
+		the_title( '<div class="post__title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></div>' );
+	?>
 
 </article>
