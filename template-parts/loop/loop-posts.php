3ea07c82f1583db85f7f540623c50ad8d371ab64
geoffgraham <geoff@geoffgraham.me>
2020-05-07 16:44:49 -0700

    Adds pagination to archives.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- template-parts/loop/loop-posts.php
+++ template-parts/loop/loop-posts.php
@@ -1,8 +1,8 @@
 
 	<section class="posts">
 
-		<?php // If "TIL" category, let's show a title.
-		if ( is_category( 'TIL' ) ):
+		<?php // If "TIL" category, let's show it's title.
+		if ( is_category( 'TIL' ) ) :
 			echo '<h1>Today I Learned...</h1>';
 		else:
 			echo '<h1>Blog</h1>';
@@ -10,18 +10,19 @@
 
 		<?php if ( have_posts() )
 			while ( have_posts() ) : the_post();
-
-			if ( is_category( 'TIL' ) ) :
-				get_template_part( 'template-parts/loop/category', 'til' );
-			else :
 				get_template_part( 'template-parts/loop/category', 'all' );
-			endif;
-
 			endwhile;
 		?>
-		<div class="posts__navigation">
-			<?php
-				echo paginate_links( array( add_fragment => '#top' ) );
+		<div class="posts__pagination">
+			<?php echo paginate_links( 
+					array( 
+						'add_fragment' => '#top',
+						'before_page_number'=> '<span class="hide" aria-hidden="true">',
+						'after_page_number'=> '</span>',
+						'next_text'    => 'Older Posts',
+						'prev_text'    => 'Newer Posts'
+					) 
+				);
 			?>
 		</div>
 
