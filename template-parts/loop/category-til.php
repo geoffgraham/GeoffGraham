cc2d132215ebe660856cb866330a758f8e032ef6
geoffgraham <geoff@geoffgraham.me>
2020-05-07 16:44:29 -0700

    Ditches custom layout for TIL category archive.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- template-parts/loop/category-til.php
+++ template-parts/loop/category-til.php
@@ -2,17 +2,7 @@
 
 	<?php the_title( '<h2 class="post-single__title">', '</h2>' ); ?>
 	<div class="post-single__date">
-		<?php 
-			echo the_date('F j, Y');
-			$j_date = get_the_date('j');
-			$j_modified_date = get_the_modified_time('j');
-			if ($j_modified_date >= $j_date + 1) { 
-				echo "<span>Updated: " . get_the_modified_time('n/d/Y') . "</span>";
-			}
-		?>
-	</div>
-	<div class="post-single__body">
-		<?php echo the_content(); ?>
+		<?php echo the_date('F j, Y'); ?>
 	</div>
 
 </article>
