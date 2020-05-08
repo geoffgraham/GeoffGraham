6e76819ac2e87952a8f18bc4521e913d9daa69f4
geoffgraham <geoff@geoffgraham.me>
2020-05-07 17:17:37 -0700

    Revises TIL date label a bit.
    Signed-off-by: geoffgraham <geoff@geoffgraham.me>

--- single.php
+++ single.php
@@ -21,15 +21,12 @@ get_header();
 					<?php 
 						$j_date = get_the_date('j');
 						$j_modified_date = get_the_modified_time('j');
-						if ( in_category('TIL') ) {
-							echo "On ";
-						}
 						echo the_date('F j, Y');
 						if ( ($j_modified_date >= $j_date + 1) && !in_category('TIL') ) { 
 							echo "<span>Updated: " . the_modified_time('n/d/Y') . "</span>";
 						}
 						if ( in_category('TIL') ) {
-							echo ", I learned...";
+							echo ", I learned";
 						}
 					?>
 				</div>
