62f8073718e97025aad17a1b440697f1990c360b
geoffgraham <geoff@geoffgraham.me>
2020-08-21 12:22:35 -0700

    Initial card component.

--- template-parts/content/content-home.php
+++ template-parts/content/content-home.php
@@ -5,19 +5,25 @@
 	</div>
 </div>
 
-<div>
-	<article>
-		<img src="<?php echo esc_url( get_template_directory_uri() . '/dist/img/symbol-picture.svg' ); ?>" alt="">
-		<h3>I'm a front-end designer</h3>
+<div class="callouts">
+	<?php
+	if( have_rows('callout') ):
+		while( have_rows('callout') ) : the_row();
+			$image = get_sub_field('callout_image');
+			$heading = get_sub_field('callout_heading');
+	?>
+	<article class="callout">
+		<div class="callout__content">
+			<img class="callout__image" src="<?php echo esc_url( $image ); ?>" alt="" />
+			<h3 class="callout__heading"><?php echo $heading ?></h3>
+		</div>
 	</article>
 
-	<article>
-		<img src="<?php echo esc_url( get_template_directory_uri() . '/dist/img/symbol-computer.svg' ); ?>" alt="">
-		<h3>I'm a front-end developer</h3>
-	</article>
+	<?php
+	endwhile;
 
-	<article>
-		<img src="<?php echo esc_url( get_template_directory_uri() . '/dist/img/symbol-actions.svg' ); ?>" alt="">
-		<h3>I'm a jack of all trades</h3>
-	</article>
-</div>
+	// No value.
+	else :
+			echo 'Sorry, nothing to see here.';
+	endif;
+?>
