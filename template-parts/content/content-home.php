c70def7a24b6f68d3b3300ee9ea1facd6ea4c302
geoffgraham <geoff@geoffgraham.me>
2020-11-13 17:54:18 -0800

    Unfinished hover effect on homepage headshot.

--- template-parts/content/content-home.php
+++ template-parts/content/content-home.php
@@ -2,7 +2,12 @@
 	<div class="slanted-container">
 		<h1 class="statement"><?php the_title() ?></h1>
 	</div>
-	<img class="home-headshot" src="<?php echo esc_url( get_template_directory_uri() . '/dist/img/headshot.svg' ); ?>">
+
+	<div class="headshot">
+		<img class="headshot-sunglasses" src="<?php echo esc_url( get_template_directory_uri() . '/dist/img/icon-sunglasses.svg' ); ?>">
+
+		<img class="home-headshot" src="<?php echo esc_url( get_template_directory_uri() . '/dist/img/headshot.svg' ); ?>">
+	</div>
 </div>
 
 <?php if ( is_front_page() && ! is_404() ) {
