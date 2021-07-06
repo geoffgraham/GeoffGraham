4f2623a2359f4f26b6a14cdcf02bbf060e418102
geoffgraham <geoff@geoffgraham.me>
2021-07-06 10:00:23 -0700

    Adds wembemtion.io meta links to header.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -10,7 +10,9 @@
  */
 function geoff_graham_pingback_header() {
 	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+		printf( '<link rel="pingback" href="%s">
+		<link rel="webmention" href="https://webmention.io/geoffgraham.me/webmention" />
+		<link rel="pingback" href="https://webmention.io/geoffgraham.me/xmlrpc" />', esc_url( get_bloginfo( 'pingback_url' ) ) );
 	}
 }
 add_action( 'wp_head', 'geoff_graham_pingback_header' );
