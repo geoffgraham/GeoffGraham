6975fe7f258d59745b8acceb9aedd3cebb7eaa8b
geoffgraham <geoff@geoffgraham.me>
2024-02-09 14:51:33 -0700

    Gravatar in one liners archive.

--- template-parts/loop/category-one-liners.php
+++ template-parts/loop/category-one-liners.php
@@ -1,11 +1,19 @@
 <article class="post">
 
-	<div class="post__date">
-		<span><?php the_date( 'M d, Y' );?></span>
-	</div>
-	<?php 
+	<?php
+    echo get_avatar( 'geoff@geoffgraham.me', 75 );
 		the_title( '<div class="post__title">', '</div>' );
     the_content();
 	?>
+
+  <footer class="post__footer">
+    <img src="
+      <?php
+        print get_avatar_url(get_current_user_id(),
+        ['size' => '30']); 
+      ?>"
+    >
+    <span class="post__date"><?php the_date( 'M d, Y' );?></span>
+  </footer>
 
 </article>
