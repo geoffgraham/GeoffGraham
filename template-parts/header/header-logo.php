48c013eec09eb4121ba1513a3c0b6dc4fda45f37
geoffgraham <geoff@geoffgraham.me>
2026-02-02 18:27:17 -0700

    Link style tinkering.

--- template-parts/header/header-logo.php
+++ template-parts/header/header-logo.php
@@ -1,4 +1,1 @@
-
-<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" aria-label="Home">
-  <img src="<?php echo get_template_directory_uri(); ?>/dist/img/geoffgraham.webp" alt="Geoff Graham in orange beanie and tongue sticking out.">
-</a>
+<img src="<?php echo get_template_directory_uri(); ?>/dist/img/geoffgraham.webp" alt="Geoff Graham in orange beanie and tongue sticking out.">
